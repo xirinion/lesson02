de582107ec9207930514186cf76129aa5457e49a
Vlad <Vlad@example.com>
2025-02-06 16:18:31 +0500

    ADD GENDER AND AGE

--- Program.cs
+++ Program.cs
@@ -4,19 +4,28 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Введите ваше имя; ");
+        Console.WriteLine("Введите ваше имя; ");
         string name = Console.ReadLine();
-        Console.Write($"ваше имя; {name}");
+        Console.WriteLine($"ваше имя; {name}");
 
-        Console.Write("Введите ваш пол(м/ж): ");
-        string gender = Console.ReadLine().Trim().ToLower();
-        if(gender =="м")
+        Console.WriteLine("Введите ваше возраст; ");
+        int age;
+        // bool isAgeInt = (int.TryParse(Console.ReadLine(), out age))
+        while(!int.TryParse(Console.ReadLine(), out age) || age < 0)
         {
-
+            Console.Write("некоректный ввод. Введите число больше либо 0 ");
+            Console.Write("Введите ваше возраст; ");
         }
-        else
-        {
-            
+        Console.Write($"ваше возраст; {age}");
+
+        // Console.Write("Введите ваш пол(м/ж): ");
+        // string gender = Console.ReadLine().Trim().ToLower();
+        // if(gender =="м")
+        // {
+
+        // }
+        // else
+        // {
+
         }
     }
-}
