9e0a92ce2b76df62e1ed406e0325d9f3d31c8aa6
Vlad <Vlad@example.com>
2025-02-06 16:17:39 +0500

    ADD Gender and AGE

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ваше имя; ");
+        string name = Console.ReadLine();
+        Console.Write($"ваше имя; {name}");
+
+        Console.Write("Введите ваш пол(м/ж): ");
+        string gender = Console.ReadLine().Trim().ToLower();
+        if(gender =="м")
+        {
+
+        }
+        else
+        {
+            
+        }
+    }
+}
